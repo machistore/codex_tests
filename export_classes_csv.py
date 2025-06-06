8bcef1525a15f82e5b49de19ea56dfe8e53fb74e
k.machi <104885577+machistore@users.noreply.github.com>
2025-06-07 02:27:36 +0900

    Handle deprecated Vectorworks class functions

--- export_classes_csv.py
+++ export_classes_csv.py
@@ -7,19 +7,55 @@ except ImportError:
     raise SystemExit('This script must be executed inside Vectorworks with the vs module.')
 
 
+def _call_vs_function(possible_names, *args):
+    """Try to call the first available Vectorworks function from
+    ``possible_names`` with ``args``.
+    ``possible_names`` should be an iterable of function name strings.
+    """
+    for name in possible_names:
+        func = getattr(vs, name, None)
+        if callable(func):
+            return func(*args)
+    raise AttributeError(
+        f"None of the following Vectorworks functions are available: {possible_names}"
+    )
+
+
 def get_class_attributes(class_name):
     """Return a dictionary with common class attributes."""
     attrs = {
         'name': class_name,
-        'use_graphic': vs.GetClUseGraphic(class_name),
-        'line_weight': vs.GetClLW(class_name),
-        'line_color_fore': vs.GetClPenFore(class_name),
-        'line_color_back': vs.GetClPenBack(class_name),
-        'line_style': vs.GetClLS(class_name),
-        'fill_style': vs.GetClFPat(class_name),
-        'fill_color_fore': vs.GetClFillFore(class_name),
-        'fill_color_back': vs.GetClFillBack(class_name),
-        'visibility': vs.GetCVis(class_name),
+        'use_graphic': _call_vs_function(
+            ['GetClassUseGraphic', 'GetClUseGraphic'], class_name
+        ),
+        'line_weight': _call_vs_function(
+            ['GetClassLW', 'GetClassLineWeight', 'GetClLW'], class_name
+        ),
+        'line_color_fore': _call_vs_function(
+            ['GetClassPenForeColor', 'GetClassPenFore', 'GetClPenFore'],
+            class_name,
+        ),
+        'line_color_back': _call_vs_function(
+            ['GetClassPenBackColor', 'GetClassPenBack', 'GetClPenBack'],
+            class_name,
+        ),
+        'line_style': _call_vs_function(
+            ['GetClassLineStyle', 'GetClassLS', 'GetClLS'], class_name
+        ),
+        'fill_style': _call_vs_function(
+            ['GetClassFPat', 'GetClassFillPat', 'GetClFPat'], class_name
+        ),
+        'fill_color_fore': _call_vs_function(
+            ['GetClassFillForeColor', 'GetClassFillFore', 'GetClFillFore'],
+            class_name,
+        ),
+        'fill_color_back': _call_vs_function(
+            ['GetClassFillBackColor', 'GetClassFillBack', 'GetClFillBack'],
+            class_name,
+        ),
+        'visibility': _call_vs_function(
+            ['GetClassVisibility', 'GetCVis'], class_name
+        ),
     }
     return attrs
 
